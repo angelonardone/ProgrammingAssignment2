4a8482039575792a0a2518aae2dcdfcc26ec3204
Angelo Nardone <angelo.nardone@gmail.com>
2014-08-24 17:06:09 -0400

    corrected minor mistakes in functions

--- cachematrix.R
+++ cachematrix.R
@@ -6,16 +6,21 @@
 makeCacheMatrix<-function(x=matrix()){
     ##This function creates a special "matrix" object that can cache its inverse.
     m <- NULL
-    mat <- function(y) {
+    # here I set the matix
+    set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    # this function get back the matrix
     get <- function() x
+    # this compute the invers of the matrix ~ solve()
     setmatinv <- function(solve) m <<- solve
+    # this returnt the computed inverse store based on scopint rules
     getmatinv <- function() m
+    # here i return the list of functions
     list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+         setmatinv = setmatinv,
+         getmatinv = getmatinv)
 }
 
 ## Write a short comment describing this function
@@ -24,12 +29,16 @@ cacheSolve <- function(x, ...) {
     ##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
     # If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
     m <- x$getmatinv()
+    # if existe I return the cahced inverse matrix
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
+    # it does not exiest so I get the matrix first
     data <- x$get()
+    # then I compute the inverse of the matrix with the ~ solve() command
     m <- solve(data, ...)
+    # i set the invers into the cache
     x$setmatinv(m)
     m
 }
